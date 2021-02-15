7d49d599792b804bcf8c1b0556057cf1eb7aec35
Cecilia Cruz <cruzscecilia@gmail.com>
2021-02-15 13:37:17 -0800

    Add project files.

---
+++ code.js
@@ -0,0 +1,9 @@
+/*clockHTML = document.getElementById("clock");
+
+function setTime () {
+	let currentTime = new Date();
+	currentTime = currentTime.toLocaleTimeString();
+	clockHTML.innerHTML = currentTime; 
+}
+
+let update = setInterval(setTime, 1000);*/
