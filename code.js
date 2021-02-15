262815ab8c1e5acbc9f592856820c832bc3739cd
Cecilia Cruz <cruzscecilia@gmail.com>
2021-02-15 13:41:03 -0800

    Updated list of holidays to include

--- code.js
+++ code.js
@@ -7,3 +7,15 @@ function setTime () {
 }
 
 let update = setInterval(setTime, 1000);*/
+
+/*Holidays:
+New Years Day
+Valentines Day
+St. Patrick's Day
+Easter
+Cinco de Mayo
+4th of July
+Halloween
+Thanksgiving
+Christmas
+*/
